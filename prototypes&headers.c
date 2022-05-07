56ae9661ec5741bf5c3797c6ee75a635b6ad15b5
FaToma245 <104271503+FaToma245@users.noreply.github.com>
2022-05-07 15:55:35 +0200

    important prototypes and headers

---
+++ prototypes&headers.c
@@ -0,0 +1,37 @@
+#include "tm4c123gh6pm.h" // include register definition file of TM4C123GH6PM 
+#define LCD GPIOB   /* Define "LCD" as a symbolic name for GPIOB */
+#define RS 0x20 /* PORTB BIT5 mask */
+#define RW 0x40 /* PORTB BIT6 mask */
+#define EN 0x80 /* PORTB BIT7 mask */
+#define HIGH 1
+#define LOW 0
+
+/*define useful symbolic names for LCD commands */
+#define clear_display     0x01
+#define returnHome        0x02
+#define moveCursorRight   0x06
+#define moveCursorLeft    0x08
+#define shiftDisplayRight 0x1C
+#define shiftDisplayLeft  0x18
+#define cursorBlink       0x0F
+#define cursorOff         0x0C
+#define cursorOn          0x0E
+#define Function_set_4bit 0x28
+#define Function_set_8bit 0x38
+#define Entry_mode        0x06
+#define Function_8_bit    0x32
+#define Set5x7FontSize    0x20
+#define FirstRow          0x80
+
+
+
+/* prototypes of LCD functions */
+void delay_ms(int n); /* mili second delay function */
+void delay_us(int n); /* micro second delay function */ 
+void LCD_init(void);  /* LCD initialization function */
+void LCD_Cmd(unsigned char command); /*Used to send commands to LCD */
+void LCD_Write_Char(unsigned char data); /* Writes ASCII character */
+void LCD_Write_Nibble(unsigned char data, unsigned char control); /* Writes 4-bits */
+void LCD_String (char *str);	/* Send string to LCD function */
+void keypad_Init(void); /* Keypad initialization */
+char keypad_getkey(void); /*Scan and detect key pressed */
