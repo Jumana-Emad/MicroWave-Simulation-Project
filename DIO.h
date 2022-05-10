4f5707181d34d805fe70d57d4f023e52c13f8e74
Jumana-Emad <jumanaelhak@gmail.com>
2022-05-10 17:40:19 +0200

    Update DIO.h

--- DIO.h
+++ DIO.h
@@ -5,3 +5,6 @@ void PORT_INIT(unsigned char portName);
 void DIO_vSETPINDIRECTION (unsigned char portName, unsigned char pinNumber, unsigned char direction);
 void DIO_vWRITEPIN (unsigned char portName, unsigned char pinNumber, unsigned char data);
 void DIO_vTOGGLEPIN(unsigned char portName, unsigned char pinNumber);
+void DIO_vSETPORTDIR(unsigned char portname, unsigned char direction);
+void DIO_vWRITEPORT(unsigned char portname , unsigned char data);
+unsigned char GPIO_u8READPIN(unsigned char portname, unsigned char pinnumber);
