60a1b689b461a5f8fa0e5922dff4ddb284dbc73b
HebaBakry <Heba.m.bakry@gmail.com>
2022-05-11 13:17:56 +0200

    Button Header file

---
+++ Button.h
@@ -0,0 +1,6 @@
+#include "Button.c"
+
+
+         void BUTTON_vINIT(unsigned char PortName, unsigned char PIN_NO);
+         unsigned char BUTTON_u8READ(unsigned char PortName, unsigned char PIN_NO);
+         void BUUTON_vINITPULLUP(unsigned char PortName, unsigned char PIN_NO);
