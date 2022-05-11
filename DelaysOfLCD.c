0dffd2b7f86a4855903dd63515fb10b7b80beb21
FaToma245 <104271503+FaToma245@users.noreply.github.com>
2022-05-11 06:43:07 +0200

    delay of ms and its components

---
+++ DelaysOfLCD.c
@@ -0,0 +1,19 @@
+#include "tm4c123gh6pm.h"
+#include "stdint.h"
+void SysTick_wait_1ms() { //best is to disable then to enable
+		NVIC_ST_CTRL_R=0;
+		NVIC_ST_RELOAD_R = 80000 - 1;
+		NVIC_ST_CURRENT_R=0;
+		NVIC_ST_CTRL_R=0x05;
+		while ((NVIC_ST_CTRL_R&0x00010000)==0){} //wait for count flag		
+	}
+	
+
+	
+ uint32_t i;
+	void delay_ms (uint32_t time) {
+	  for (i=0; i<time; i++){
+	    SysTick_wait_1ms(); 
+	  }
+	  
+	}
