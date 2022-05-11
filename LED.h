0f9bed94236d812e847357efeb619b1cf06abd6d
HebaBakry <102521383+HebaBakry@users.noreply.github.com>
2022-05-11 12:50:48 +0200

    Delete LED.h

--- LED.h
+++
@@ -1,7 +0,0 @@
-#include "LED.c"
-
-
-void LED_vINIT(unsigned char PortName, unsigned char PIN_NO);
-void LED_vON(unsigned char PortName, unsigned char PIN_NO);
-void LED_vOff(unsigned char PortName, unsigned char PIN_NO);
-void LED_vToggle(unsigned char PortName, unsigned char PIN_NO);
