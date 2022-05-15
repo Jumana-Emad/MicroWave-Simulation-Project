57409b2eec697e2a3bebc6dd2516ac47b324cf53
Martina2911 <104918343+Martina2911@users.noreply.github.com>
2022-05-15 17:05:56 +0200

    Seven Commit

--- Systic Counter.h
+++
@@ -1,5 +0,0 @@
-#include "tm4c123gh6pm.h"
-#include "stdint.h"
-void SysTickWait1msec();
-void count_msec(uint32_t Time);
-void count_sec(uint32_t Time);	
---
+++ Systick Counter.h
@@ -0,0 +1,5 @@
+#include "tm4c123gh6pm.h"
+#include "stdint.h"
+void SysTickWait1msec();
+void count_msec(uint32_t Time);
+void count_sec(uint32_t Time);	
