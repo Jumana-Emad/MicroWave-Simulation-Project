1eb0773a4013c9301338af31005134984ee8ad1a
Monica 1900199 <104925298+MonicaMickie@users.noreply.github.com>
2022-05-16 14:16:00 +0200

    Update KeyPad.c

--- KeyPad.c
+++ KeyPad.c
@@ -3,10 +3,10 @@
 void Keypad_Init()
 {
 	//PORT_INIT(PortName);
-	DIO_vSETPINDIRECTION('D', 0, 1);
-	DIO_vSETPINDIRECTION('D', 1, 1);
-	DIO_vSETPINDIRECTION('D', 2, 1);
-	DIO_vSETPINDIRECTION('D', 3, 1);
+	DIO_vSETPINDIRECTION('E', 0, 1);
+	DIO_vSETPINDIRECTION('E', 1, 1);
+	DIO_vSETPINDIRECTION('E', 2, 1);
+	DIO_vSETPINDIRECTION('E', 3, 1);
 	DIO_vSETPINDIRECTION('A', 4, 0);
 	DIO_vSETPINDIRECTION('A', 5, 0);
 	DIO_vSETPINDIRECTION('A', 6, 0);
@@ -29,8 +29,8 @@ unsigned char KeyPad_u8Read(unsigned char PortName)
 	char value_Returned = 0xFF;
 	for (Row = 0; Row < 4; Row++)
 		{
-			DIO_uWriteLowNibble('D', 0xFF);
-			DIO_vWRITEPIN ('D', Row, 0);
+			DIO_uWriteLowNibble('E', 0xFF);
+			DIO_vWRITEPIN ('E', Row, 0);
 			for(Column = 4; Column < 8; Column++)
 			{
 				x = GPIO_u8READPIN('A', Column);
