98b7377242a17e8905fcd1268e2fb50fd53ff760
FaToma245 <104271503+FaToma245@users.noreply.github.com>
2022-05-16 20:05:44 +0200

    Update LCD.h

--- LCD.h
+++ LCD.h
@@ -4,8 +4,9 @@
 static void Send_Pulse(void);
 void LCD_Send_cmd(char cmd);
 void LCD_vInit(void);
-void LCD_Send_char(char chr);
+void LCD_Send_char(unsigned char chr);
 void Send_string(char *data);
+void LCD_Write4bits(unsigned char data, unsigned char control);
 void LCD_clearscreen(void);
 void LCD_MoveCursor(char row, char col);
 void Lcd_Shift_Right();
