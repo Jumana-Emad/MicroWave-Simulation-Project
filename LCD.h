ed7863bb2501faed71765db79ae89b12ed48535f
HebaBakry <102521383+HebaBakry@users.noreply.github.com>
2022-05-18 15:03:41 +0200

    Update LCD.h

--- LCD.h
+++ LCD.h
@@ -6,6 +6,7 @@ void LCD_Send_cmd(char cmd);
 void LCD_vInit(void);
 void LCD_Send_char(unsigned char chr);
 void Send_string(char *data);
+void LCD_Count(char* str);
 void LCD_String(char* str);
 void LCD_Write4bits(unsigned char data, unsigned char control);
 void LCD_clearscreen(void);
