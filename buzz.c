c07a1ab7f53b957b742651ccef4524acada7f592
HebaBakry <102521383+HebaBakry@users.noreply.github.com>
2022-05-23 16:01:40 +0200

    Update buzz.c

--- buzz.c
+++ buzz.c
@@ -16,3 +16,36 @@ void Buzz_vOff(){
 void Buzz_vToggle(){
 	DIO_vTOGGLEPIN('A', 6);
 }
+
+
+
+void End()
+{
+DIO_vSETPINDIRECTION('E',4,1);
+	GPIO_PORTE_DATA_R|=0x10;
+	for (z=0;z<7;z++){
+	LED_vToggle('F' , 1);
+	LED_vToggle('F' , 2);
+	LED_vToggle('F' , 3);
+	delay_ms(500);	//delay0.5sec
+	}										
+
+
+}
+
+
+
+void End_D()
+{
+DIO_vSETPINDIRECTION('E',4,1);
+	
+	for (z=0;z<100;z++){
+	LED_vToggle('F' , 1);
+	LED_vToggle('F' , 2);
+	LED_vToggle('F' , 3);
+	delay_ms(500);							//delay0.5sec
+	if(BUTTON_u8READ('F',0)==0){break;}
+	}							
+
+		
+}
