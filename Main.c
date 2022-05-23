c78ed57a86738486bc4bec7f74c1415a079679bf
Marinaa2002 <104918694+Marinaa2002@users.noreply.github.com>
2022-05-23 16:20:47 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -6,3 +6,35 @@
 #include "LED.h"
 #include "Button.h"
 #include "buzz.h"
+enum {Idle,Cooking,Weighing,Get_Time};
+void RGB_OUTPUT (unsigned char data) {
+   GPIO_PORTF_DATA_R &=~ 0x0E;
+	 GPIO_PORTF_DATA_R |= data;
+}
+
+int main(){
+char* str ="";
+char time[5];
+int weight;	
+int case_d_time ;
+int i;	 
+int input;
+int State = Idle;
+char Returned_Value;
+		
+	
+while(1){
+if ((BUTTON_u8READ('A',7)) !=0){ //not pressed door closed
+switch(State){
+		case(Idle):
+		str = "Idle";
+		LCD_String(str);
+		str = "Enter Ur choice";
+		LCD_String(str);		
+		Returned_Value = KeyPad_u8Read();
+		if (Returned_Value =='A'){State=Cooking;}		
+		else if ((Returned_Value =='B'||Returned_Value =='C')){State = Weighing;}
+		else if	(Returned_Value =='D'){State = Get_Time;}
+		//else {State = Idle;}
+		break;
+       
