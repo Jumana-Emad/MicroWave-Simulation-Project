783b6aa98e3a3ee551231e4d12c6438399012bd2
FaToma245 <104271503+FaToma245@users.noreply.github.com>
2022-05-23 15:58:53 +0200

    Update Delays.c

--- Delays.c
+++ Delays.c
@@ -3,7 +3,10 @@
 #include "Math.h"
 #include "LCD.h"
 #include "string.h"
-
+#include "Button.h"
+#include "LED.h"
+#include "buzz.h"
+int back=0;
 void SysTick_wait_1ms() { //best is to disable then to enable
 		NVIC_ST_CTRL_R=0;
 		NVIC_ST_RELOAD_R = 16000 - 1;
@@ -48,8 +51,19 @@ char* formatTime(int time_seconds){
 
 uint32_t j;
 	void delay_s(uint32_t Time) {
+	BUUTON_vINITPULLUP('A', 7);
+		LED_vINIT('F',1);
+		LED_vINIT('F',2);
+		LED_vINIT('F',3);
+		LED_vON('F',1);
+		LED_vON('F',2);
+		LED_vON('F',3);
 	  for (j=Time; j>0 ; j--){
 	    delay_ms(1000);
-	    Send_string(formatTime(j));	  
+	if (((BUTTON_u8READ('A',7)) ==0) || ((BUTTON_u8READ('F',4)) == 0)){
+				 back = j;			
+				break;
+			}
+	    LCD_Count(formatTime(j));	  
 	  }
 	}
